ccb7debdcae1aab6c6b1bf3c9c17e64ff8faabf6
DESKTOP-3KDESHE\승아 <ysa8497@gmail.com>
2020-08-19 22:50:20 +0900

    BaekJoon Online Judge 11718
    그대로 출력하기

---
+++ IO/IO_11718.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class IO_11718 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan=new Scanner(System.in);
+		String str;
+		
+		while(scan.hasNext()) {
+			str=scan.nextLine();
+			System.out.println(str);
+		}
+	}
+
+}
