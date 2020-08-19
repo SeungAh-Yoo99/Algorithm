86beeefa46102b9670c92329b3a9ff05d38d6b6e
DESKTOP-3KDESHE\승아 <ysa8497@gmail.com>
2020-08-20 01:59:25 +0900

    BaekJoon Online Judge 10171
    고양이

---
+++ IO/IO_10171.java
@@ -0,0 +1,14 @@
+/*
+'\'를 출력하고 싶다면 "\\"로 해주어야 한다.
+ */
+public class IO_10171 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
