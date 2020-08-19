90d8adc8b5e6d3187c41963ba776e9f536d42a55
DESKTOP-3KDESHE\승아 <ysa8497@gmail.com>
2020-08-20 02:10:07 +0900

    BaekJoon Online Judge 2588
    곱셈

---
+++ IO/IO_2588.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class IO_2588 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		int num1=scan.nextInt();
+		int num2=scan.nextInt();
+		int num3=num1*(num2%10);
+		int num4=num1*(num2%100/10);
+		int num5=num1*(num2/100);
+		
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(num5);
+		System.out.println(num1*num2);
+	}
+
+}
