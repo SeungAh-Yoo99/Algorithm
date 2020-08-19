afecca1b198f2144e20e4578b687de029b42a55a
DESKTOP-3KDESHE\승아 <ysa8497@gmail.com>
2020-08-20 02:03:03 +0900

    BaekJoon Online Judge 1001
    A-B

---
+++ IO/IO_1001.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class IO_1001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		int sub=a-b;
+		System.out.println(sub);
+	}
+
+}
