a19671cfe8443395470d49d84411979b5babe147
DESKTOP-3KDESHE\승아 <ysa8497@gmail.com>
2020-08-19 22:51:24 +0900

    BaekJoon Online Judge 11719
    그대로 출력하기 2

---
+++ IO/IO_11719.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class IO_11719 {
+
+	public static void main(String args[]) {
+		
+		Scanner scan=new Scanner(System.in);
+		String str;
+		
+		while(scan.hasNext()) {
+			str=scan.nextLine();
+			System.out.println(str);
+		}
+	}
+}
