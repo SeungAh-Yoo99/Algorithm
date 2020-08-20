a79801ce89b20863731cb88369bacafbde7813ce
DESKTOP-3KDESHE\승아 <ysa8497@gmail.com>
2020-08-20 16:58:37 +0900

    BaekJoon Online Judge 1330
    두 수 비교하기

---
+++ IO/IO_1330.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class IO_1330 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		
+		if (a>b)
+			System.out.println(">");
+		else if (a<b)
+			System.out.println("<");
+		else
+			System.out.println("==");
+	}
+
+}
