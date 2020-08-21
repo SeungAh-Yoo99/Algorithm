ecbe462c7d1d159c08d95425ce3864511de5665c
DESKTOP-3KDESHE\승아 <ysa8497@gmail.com>
2020-08-21 22:25:40 +0900

    BaekJoon Online Judge 10871
    X보다 작은 수

---
+++ IO/IO_10871.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class IO_10871 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int x=scan.nextInt();
+		int a[]=new int[n];
+		
+		for (int i=0;i<n;i++)
+			a[i]=scan.nextInt();
+		
+		for (int i=0;i<n;i++) {
+			if (a[i]<x)
+				System.out.print(a[i]+" ");
+		}
+		System.out.println();
+	}
+
+}
