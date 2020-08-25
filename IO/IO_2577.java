1cbf39d404b5f63164087440d709c7b207a59dbb
DESKTOP-3KDESHE\승아 <ysa8497@gmail.com>
2020-08-26 00:22:47 +0900

    BaekJoon Online Judge 2577
    숫자의 개수

---
+++ IO/IO_2577.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class IO_2577 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		int c=scan.nextInt();
+		int arr[]=new int[10];
+		int mul=a*b*c;
+		int num;
+		
+		for (int i=1;i<=mul;i*=10) {
+			num=(mul%(i*10))/i;
+			arr[num]++;
+		}
+		
+		for (int i=0;i<10;i++)
+			System.out.println(arr[i]);
+	}
+
+}
