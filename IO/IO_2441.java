7ac38424f1fed4679059618516d0a7d2a996e6e7
DESKTOP-3KDESHE\승아 <ysa8497@gmail.com>
2020-08-25 23:39:04 +0900

    BaekJoon Online Judge 2441
    별 찍기 - 4

---
+++ IO/IO_2441.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class IO_2441 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan=new Scanner(System.in);
+		int count=scan.nextInt();
+		
+		for (int i=count;i>=1;i--) {
+			for (int j=1;j<=count-i;j++)
+				System.out.print(" ");
+			for (int j=1;j<=i;j++)
+				System.out.print("*");
+			System.out.println();
+		}
+	}
+
+}
