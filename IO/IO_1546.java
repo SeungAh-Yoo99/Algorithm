5d66dabf3d0aeb8d4b36936c47cd4dc99609eafb
DESKTOP-3KDESHE\승아 <ysa8497@gmail.com>
2020-08-26 00:50:28 +0900

    BaekJoon Online Judge 1546
    평균

---
+++ IO/IO_1546.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class IO_1546 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		double score[]=new double[n];
+		double max=0;
+		double avg=0;
+		
+		for (int i=0;i<n;i++) {
+			score[i]=scan.nextDouble();
+			max=Math.max(max,score[i]);
+		}
+		
+		for (int i=0;i<n;i++) {
+			score[i]=score[i]/max*100;
+			avg+=score[i];
+		}
+		avg/=n;
+		System.out.println(avg);
+	}
+
+}
