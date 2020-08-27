7daafea891196e168cbca416f09247c8ed2d065d
SeungAh-Yoo99 <ysa8497@gmail.com>
2020-08-27 18:58:52 +0900

    BaekJoon Online Judge 11654
    아스키 코드

---
+++ IO/IO_11654.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class IO_11654 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		String s=scan.next();
+		char c=s.charAt(0);
+		int ascii=(int)c;
+		System.out.println(ascii);
+	}
+
+}
