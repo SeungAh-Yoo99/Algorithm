b2e910ac965f1a0f3004217803460d126d5d0680
SeungAh-Yoo99 <ysa8497@gmail.com>
2020-09-04 22:30:59 +0900

    BaekJoon Online Judge 10250
    ACM 호텔

---
+++ IO/IO_10250.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class IO_10250 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int t = scan.nextInt();
+		int room[] = new int[t];
+		
+		for (int i=0; i<t; i++) {
+			int h = scan.nextInt();
+			int w = scan.nextInt();
+			int n = scan.nextInt();
+			int height;
+			int width;
+			
+			if (n%h == 0) {
+				height = h;
+				width = n/h;
+			}
+			else {
+				height = n%h;
+				width = n/h+1;
+			}
+			
+			room[i]=height*100+width;
+		}
+		
+		for (int i=0; i<t; i++)
+			System.out.println(room[i]);
+	}
+
+}
