2600a2cd52d9bf9c464aff4bb14387da29d3e1f9
SEUNGAH YOO <ysa8497@gmail.com>
2021-12-01 01:23:57 +0900

    카드2

---
+++ BOJ/BOJ_2164.py
@@ -0,0 +1,33 @@
+# # queue의 index가 홀수(1, 3, 5 ...)면 뒤로 보내고
+# # 짝수(0, 2, 4 ...)면 삭제함
+
+# n = int(input())
+
+# queue = [i+1 for i in range(n)]
+
+# while(len(queue) > 1):
+#     # 뒤로 보내야 할 수들이 꼭 홀수번에 오도록 만들어준다.
+#     if len(queue) % 2 == 0:
+#         queue = queue[1::2]
+#     else:
+#         q = [queue[-1]]
+#         q.extend(queue[1::2])
+#         queue = q
+        
+# print(queue[0])
+
+import sys
+from collections import deque
+
+n = int(input())
+
+queue = [i+1 for i in range(n)]
+dque = deque(queue)
+
+while(len(dque) > 1):
+    dque.popleft()
+    
+    if len(dque) > 1:
+        dque.append(dque.popleft())
+    
+print(dque.pop())
