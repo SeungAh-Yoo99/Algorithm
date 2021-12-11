ffb3ff9f0f8c530080be45bd56e3ec3746924421
SEUNGAH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2021-12-11 21:30:57 +0900

    TV 크기

---
+++ BOJ/BOJ_1297.py
@@ -0,0 +1,16 @@
+import sys
+import math
+
+D, H, W = map(int, sys.stdin.readline().split())
+
+X = math.sqrt((D ** 2) / (H ** 2 + W ** 2))
+
+h = math.trunc(H * X)
+w = math.trunc(W * X)
+
+# X = D / ((H ** 2 + W ** 2) ** 0.5)
+
+# h = int(H * X)
+# w = int(W * X)
+
+print(h, w)
