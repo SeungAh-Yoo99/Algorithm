bdca37cf143c031a4d77abe2bd768b387832bf68
SEUNGAH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2021-12-15 00:58:00 +0900

    이항 계수 1

---
+++ BOJ/BOJ_11050.py
@@ -0,0 +1,15 @@
+# import math
+
+N, K = map(int, input().split())
+
+sum1 = 1
+for num in range(N, N - K, -1):
+    sum1 *= num
+
+sum2 = 1
+for num in range(1, K + 1):
+    sum2 *= num
+
+print(sum1 // sum2)
+
+# print(math.factorial(N) // (math.factorial(K) * math.factorial(N - K)))
