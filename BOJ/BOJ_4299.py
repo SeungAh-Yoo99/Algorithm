ef7b34457d921cb601918d582bb6265c3ad44b4b
SEUNGAH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2021-12-14 23:59:09 +0900

    AFC 윔블던

--- BOJ/BOJ_4299.py
+++ BOJ/BOJ_4299.py
@@ -2,7 +2,7 @@ sum, sub = map(int, input().split())
 
 # sum과 sub가 2로 나누어 떨어지지 않으면, 점수가 자연수가 아니게 된다.
 # 문제에 sum이 항상 sub보다 크다는 조건이 없다.
-if ((sum + sub) % 2 != 0) or (sum - sub < 0):
+if ((sum + sub) % 2 != 0) or (sum < sub):
     print(-1)
     
 else:
