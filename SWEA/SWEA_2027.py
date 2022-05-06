77b371de0fba26d537dc9a9b6c65095671df69f8
SEUNGAH YOO <ysa8497@gmail.com>
2022-05-06 19:27:33 +0900

    대각선 출력하기

---
+++ SWEA/SWEA_2027.py
@@ -0,0 +1,10 @@
+# print("#++++")
+# print("+#+++")
+# print("++#++")
+# print("+++#+")
+# print("++++#")
+
+for i in range(5):
+    print("+" * i, end='')
+    print("#", end='')
+    print("+" * (4 - i))
