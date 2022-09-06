8e937999031452ab4fdaf37c073aeb3890b6cbcd
SEUNGAH YOO <ysa8497@gmail.com>
2022-09-06 18:20:25 +0900

    시각 덧셈

---
+++ SWEA/SWEA_1976.py
@@ -0,0 +1,9 @@
+T = int(input())
+
+for test_case in range(1, T + 1):
+    h1, m1, h2, m2 = map(int, input().split())
+
+    m = (m1 + m2) % 60
+    h = ((h1 + h2 + ((m1 + m2) // 60) - 1) % 12) + 1
+
+    print("#{} {} {}".format(test_case, h, m))
