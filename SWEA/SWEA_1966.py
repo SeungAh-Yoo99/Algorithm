38dd49a2269f8a7e0425e093d9252abb4e14c3c5
SEUNGAH YOO <ysa8497@gmail.com>
2022-09-07 16:22:49 +0900

    숫자를 정렬하자

---
+++ SWEA/SWEA_1966.py
@@ -0,0 +1,8 @@
+T = int(input());
+
+for test_case in range(1, T + 1):
+    N = int(input())
+    arr = sorted(list(map(int, input().split())))
+
+    print("#{}".format(test_case), end=" ")
+    print(*arr)
