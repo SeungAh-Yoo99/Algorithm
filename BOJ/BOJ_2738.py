cc52d28eaae7dc0ff82d9ec2091002806fd0de58
SEUNGAH YOO <ysa8497@gmail.com>
2022-09-12 23:13:56 +0900

    행렬 덧셈

---
+++ BOJ/BOJ_2738.py
@@ -0,0 +1,9 @@
+N, M = map(int, input().split())
+
+A = [list(map(int, input().split())) for _ in range(N)]
+B = [list(map(int, input().split())) for _ in range(N)]
+
+for n in range(N):
+    for m in range(M):
+        A[n][m] += B[n][m]
+    print(*A[n])
