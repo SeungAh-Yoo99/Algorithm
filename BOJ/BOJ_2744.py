22c7fca3dcb6f3f29824889ad7c1ee594e791186
SEUNGAH YOO <ysa8497@gmail.com>
2022-09-14 23:40:13 +0900

    대소문자 바꾸기

--- BOJ/BOJ_2744.py
+++ BOJ/BOJ_2744.py
@@ -1,17 +1,17 @@
 string = input()
-# result = ""
+result = ""
 
-# for s in string:
-#     #대문자라면
-#     if ord(s) >= ord('A') and ord(s) <= ord('Z'):
-#         result += chr(ord(s) - ord('A') + ord('a'))
-#     #소문자라면
-#     else:
-#         result += chr(ord(s) - ord('a') + ord('A'))
+for s in string:
+    #대문자라면
+    if ord(s) >= ord('A') and ord(s) <= ord('Z'):
+        result += chr(ord(s) - ord('A') + ord('a'))
+    #소문자라면
+    else:
+        result += chr(ord(s) - ord('a') + ord('A'))
 
-# print(result)
+print(result)
 
-# print(string.swapcase())
+print(string.swapcase())
 
 result = []
 
