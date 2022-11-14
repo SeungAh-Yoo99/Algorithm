3debcdc4e3b8e48d99e8b46a573b0a2061940aa6
SEUNGAH YOO <ysa8497@gmail.com>
2022-11-14 15:51:38 +0900

    통나무 자르기

---
+++ SWEA/SWEA_14692.py
@@ -0,0 +1,11 @@
+# test case tc
+tc = int(input())
+
+for test_case in range(1, tc + 1):
+	# 통나무 길이 n
+    n = int(input())
+
+    if n % 2 == 0:
+        print("#{} Alice".format(test_case))
+    else:
+        print("#{} Bob".format(test_case))
