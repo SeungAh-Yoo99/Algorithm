103c66a3903166f60aeb6115c17bcafb9785eb43
SEUNGAH YOO <ysa8497@gmail.com>
2022-11-17 00:48:40 +0900

    격자판 칠하기

--- SWEA/SWEA_14413.py
+++ SWEA/SWEA_14413.py
@@ -2,42 +2,51 @@
 t = int(input())
 
 for test_case in range(1, t + 1):
-	# 행의 수 n, 열의 수 m 입력
-	n, m = map(int, input().split())
-	# 2차원 배열의 동, 남 이동
-	dx = [0, 1]
-	dy = [1, 0]
+    # 행의 수 n, 열의 수 m 입력
+    n, m = map(int, input().split())
 
 	# 문자열 입력
-	array = []
-	for i in range(n):
-		array.append(list(input()))
+    array = []
+    for _ in range(n):
+        array.append(list(input()))
 
-	result = ''
-	for i in range(n):
-		for j in range(m):
-			# 동쪽, 남쪽 중 하나라도 같은 문자라면 답은 imposiible
-			# 모두 다른 문자라면 continue
-			# ?라면 #일 경우 .을, .일 경우 #를 찍어준다.
-			for k in range(2):
-				# 인덱스 범위 체크
-				if i + dx[k] < n and j + dy[k] < m:
-					if array[i + dx[k]][j + dy[k]] == '?':
-						if array[i][j] == '#':
-							array[i + dx[k]][j + dy[k]] = '.'
-						elif array[i][j] == '.':
-							array[i + dx[k]][j + dy[k]] = '#'
-					elif array[i][j] == array[i + dx[k]][j + dy[k]]:
-						result = "impossible"
-						break
-			# 답이 이미 impossible이면 반복문 빠져 나오게 하기
-			if result == "impossible":
-				break
-		# 답이 이미 impossible이면 반복문 빠져 나오게 하기
-		if result == "impossible":
-			break
+    # array[i][j]일 때 i + j가 짝수일 때의 문자와 홀수일 때의 문자
+    for i in range(n * m):
+        if array[i // m][i % m] == '#':
+            if ((i // m) + (i % m)) % 2 == 0:
+                even = '#'
+                odd = '.'
+            else:
+                even = '.'
+                odd = '#'
+            break
+        elif array[i // m][i % m] == '.':
+            if i % 2 == 0:
+                even = '.'
+                odd = '#'
+            else:
+                even = '#'
+                odd = '.'
+            break
 
-	if result != 'impossible':
-		result = 'possible'
+    # array[i][j]일 때, i + j가 홀수일 때는 odd, 짝수일 때는 even과 문자열이 같아야 한다.
+    result = ''
+    for i in range(n * m):
+        # 짝수일 때
+        if ((i // m) + (i % m)) % 2 == 0:
+            # odd와 문자열이 같다면 실패
+            if array[i // m][i % m] == odd:
+                result = "impossible"
+                break
+        # 홀수일 때
+        else:
+            # even과 문자열이 같다면 실패
+            if array[i // m][i % m] == even:
+                result = "impossible"
+                break
 
-	print("#{} {}".format(test_case, result))
+    # 반복문이 끝날 때까지 result가  impossible이 아니라면 result는 possible
+    if result != "impossible":
+        result = "possible"
+
+    print("#{} {}".format(test_case, result))
