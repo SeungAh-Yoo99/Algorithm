3656e210ea2ca46c4a6352f7f5ff0c7340937968
SEUNGAH YOO <ysa8497@gmail.com>
2023-01-12 23:45:14 +0900

    팩토리얼 0의 개수

---
+++ BOJ/BOJ_1676.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ 
+public class BOJ_1676 {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int cnt = 0;
+		
+		while(n >= 5) {
+			cnt += n / 5;
+			n /= 5;
+		}
+		
+		System.out.println(cnt);
+	}
+ 
+}
