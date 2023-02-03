8be04236ce18c12c2dd91defa4b1ac38396fde87
SEUNGAH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2023-02-03 15:58:56 +0900

    [Silver III] Title: 카드 놓기, Time: 924 ms, Memory: 172032 KB -BaekjoonHub

--- 백준/Silver/18115. 카드 놓기/카드 놓기.java
+++ 백준/Silver/18115. 카드 놓기/카드 놓기.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Deque;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
@@ -10,37 +9,37 @@ public class Main {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+		LinkedList<Integer> result = new LinkedList<>();
 		
-		// n 입력, result 배열 초기화
+		// n과 수열 A 입력, card 배열 초기화
 		int n = Integer.parseInt(br.readLine());
-		Deque<Integer> dq = new LinkedList<>();
-
-		// 카드 기술 입력
-		st = new StringTokenizer(br.readLine());
 		int[] arr = new int[n];
-		for(int i = 0; i < n; i++) {
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		for(int i = n - 1, j = 1; i >= 0; i--, j++) {
+		// 수행했던 기술을 반대로 보면서 카드를 가져가면 된다.
+		for (int i = n - 1, j = 1; i >= 0; i--, j++) {
+			// 맨 위에 카드를 둠
 			if(arr[i] == 1) {
-				dq.addLast(j);
+				result.add(0, j);
 			}
+			// 맨 앞에서 두 번째에 카드를 둠
 			else if(arr[i] == 2) {
-				int tmp = dq.removeLast();
-				dq.addLast(j);
-				dq.addLast(tmp);
+				result.add(1, j);
 			}
-			else { // arr[i] == 3
-				dq.addFirst(j);
+			// 마지막에 카드를 둠
+			else if(arr[i] == 3) {
+				result.add(j);
 			}
 		}
 		
-		
 		// 출력
 		StringBuilder sb = new StringBuilder();
-		for (int i = n - 1; i >= 0; i--) {
-			sb.append(dq.removeLast() + " ");
+		for (int i : result) {
+			sb.append(i + " ");
 		}
 		System.out.println(sb);
 	}
