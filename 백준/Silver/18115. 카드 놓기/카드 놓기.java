607d420fba36746a8a086da7cebcf6a550108851
SEUNGAH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2023-02-03 16:01:09 +0900

    [Silver III] Title: 카드 놓기, Time: 820 ms, Memory: 135236 KB -BaekjoonHub

--- 백준/Silver/18115. 카드 놓기/카드 놓기.java
+++ 백준/Silver/18115. 카드 놓기/카드 놓기.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
+
+
 
 public class Main {
 
@@ -9,37 +10,37 @@ public class Main {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		LinkedList<Integer> result = new LinkedList<>();
 		
-		// n과 수열 A 입력, card 배열 초기화
+		// n과 수열 A 입력, result 배열 초기화
 		int n = Integer.parseInt(br.readLine());
 		int[] arr = new int[n];
+		int[] result = new int[n];
 		
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		// 수행했던 기술을 반대로 보면서 카드를 가져가면 된다.
-		for (int i = n - 1, j = 1; i >= 0; i--, j++) {
-			// 맨 위에 카드를 둠
+		int card1 = 0, card2 = 1, card3 = n - 1;
+		for(int i = 0, j = n; i < n; i++, j--) {
 			if(arr[i] == 1) {
-				result.add(0, j);
+				result[card1] = j;
+				card2++;
+				card1 = card2 - 1;
 			}
-			// 맨 앞에서 두 번째에 카드를 둠
 			else if(arr[i] == 2) {
-				result.add(1, j);
+				result[card2++] = j;
 			}
-			// 마지막에 카드를 둠
-			else if(arr[i] == 3) {
-				result.add(j);
+			else {
+				result[card3--] = j;
 			}
 		}
 		
+		
 		// 출력
 		StringBuilder sb = new StringBuilder();
-		for (int i : result) {
-			sb.append(i + " ");
+		for (int i = 0; i < n; i++) {
+			sb.append(result[i] + " ");
 		}
 		System.out.println(sb);
 	}
