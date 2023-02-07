dde990964612710890962abfe88aaa787518803e
SEUNGAH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2023-02-07 14:12:48 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 108 ms, Memory: 48,036 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -1,18 +1,38 @@
-for test_case in range(1, 11):
-    # 덤프의 횟수 n
-    n = int(input())
- 
-    # 상자의 높이값
-    height = list(map(int, input().split()))
- 
-    # 덤프의 횟수만큼 반복
-    for i in range(n):
-        # 높이값 정렬
-        height.sort()
- 
-        height[0] += 1
-        height[99] -= 1
- 
-    height.sort()
-    result = height[99] - height[0]
-    print("#{} {}".format(test_case, result))
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		// 테스트 케이스만큼 반복
+		for (int test_case = 1; test_case <= 10; test_case++) {
+			// 덤프 횟수 입력
+			int n = Integer.parseInt(br.readLine());
+			
+			// 상자의 높이값 입력
+			int[] box = new int[100];
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < 100; i++) {
+				box[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			// n만큼 덤프 진행
+			for (int i = 0; i < n; i++) {
+				Arrays.sort(box);
+				box[0] += 1;
+				box[99] -= 1;
+			}
+			
+			// 출력
+			Arrays.sort(box);
+			System.out.printf("#%d %d\n", test_case, box[99] - box[0]);
+		}
+	}
+
+}
