80de6cb681c44629b72a6007d725246882c5cc4c
SEUNGAH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2023-02-09 14:36:51 +0900

    [Silver II] Title: DNA 비밀번호, Time: 312 ms, Memory: 21944 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -3,6 +3,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+	
+	static char[] str;
+	static int[] ACGT = new int[4];
+	static int[] acgt = new int[4];
+	
+	private static void addChr(int idx) {
+		if(str[idx] == 'A') acgt[0]++;
+		else if(str[idx] == 'C') acgt[1]++;
+		else if(str[idx] == 'G') acgt[2]++;
+		else acgt[3]++;
+	}
+	
+	private static void subChr(int idx) {
+		if(str[idx] == 'A') acgt[0]--;
+		else if(str[idx] == 'C') acgt[1]--;
+		else if(str[idx] == 'G') acgt[2]--;
+		else acgt[3]--;
+	}
+	
+	private static boolean check() {
+		for (int j = 0; j < 4; j++) {
+			if(ACGT[0] > acgt[0]) return false;
+			if(ACGT[1] > acgt[1]) return false;
+			if(ACGT[2] > acgt[2]) return false;
+			if(ACGT[3] > acgt[3]) return false;
+		}
+		return true;
+	}
 
 	public static void main(String[] args) throws Exception{
 
@@ -15,11 +43,10 @@ public class Main {
 		int P = Integer.parseInt(st.nextToken());
 		
 		// 문자열 입력
-		char[] str = br.readLine().toCharArray();
+		str = br.readLine().toCharArray();
 		
 		// 문자 최소 개수
 		st = new StringTokenizer(br.readLine());
-		int[] ACGT = new int[4];
 		for (int i = 0; i < 4; i++) {
 			ACGT[i] = Integer.parseInt(st.nextToken());
 		}
@@ -28,45 +55,21 @@ public class Main {
 		int count = 0;
 		
 		// 인덱스 0 ~ P - 1까지의 문자열의 문자 갯수 구하고 확인
-		int[] acgt = new int[4];
 		for (int i = 0; i < P; i++) {
-			if(str[i] == 'A') acgt[0]++;
-			else if(str[i] == 'C') acgt[1]++;
-			else if(str[i] == 'G') acgt[2]++;
-			else acgt[3]++;
+			addChr(i);
 		}
-		boolean check = true;
-		for (int i = 0; i < 4; i++) {
-			if(ACGT[0] > acgt[0]) check = false;
-			if(ACGT[1] > acgt[1]) check = false;
-			if(ACGT[2] > acgt[2]) check = false;
-			if(ACGT[3] > acgt[3]) check = false;
-		}
-		if(check) count++;
+		if(check()) count++;
 		
 		// 한 칸씩 앞으로 가며 갯수 확인
 		for (int i = P; i < S; i++) {
 			// 추가해준 문자 개수 + 1
-			if(str[i] == 'A') acgt[0]++;
-			else if(str[i] == 'C') acgt[1]++;
-			else if(str[i] == 'G') acgt[2]++;
-			else acgt[3]++;
+			addChr(i);
 			
 			// 빼준 문자 개수 - 1
-			if(str[i - P] == 'A') acgt[0]--;
-			else if(str[i - P] == 'C') acgt[1]--;
-			else if(str[i - P] == 'G') acgt[2]--;
-			else acgt[3]--;
+			subChr(i - P);
 			
 			// 가능 여부 확인
-			check = true;
-			for (int j = 0; j < 4; j++) {
-				if(ACGT[0] > acgt[0]) check = false;
-				if(ACGT[1] > acgt[1]) check = false;
-				if(ACGT[2] > acgt[2]) check = false;
-				if(ACGT[3] > acgt[3]) check = false;
-			}
-			if(check) count++;
+			if(check()) count++;
 		}
 		
 		// 답 출력
