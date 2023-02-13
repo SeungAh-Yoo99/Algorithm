d545d02750be8832c440a0c87f791b865e4bdbe0
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2023-02-13 17:25:27 +0900

    Update 빗물.java

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -25,26 +25,26 @@ public class Main {
 		int result = 0;
 		int start = 0, end = 0;
 		for (start = 0; start < W - 1;) {
-			if (arr[start] == 0) {
+			if (arr[start] == 0) { // 0이 아닌 곳까지 이동
 				start++;
 				continue;
 			}
 			
-			int max_length = arr[start];
-w1:			while(max_length >= 1) {
-				for (end = start + 1; end < W; end++) {
-					if(arr[end] >= max_length) {
-						int max = arr[start] < arr[end] ? arr[start] : arr[end];
-						for (int i = start + 1; i < end; i++) {
+			int max_length = arr[start]; // 왼쪽 벽의 크기가 물을 고이게 만들 수 있는 최대 높이
+w1:			while(max_length >= 1) { // 최대 높이를 한 칸 씩 내려가며 오른쪽 벽의 최대 높이를 구한다
+				for (end = start + 1; end < W; end++) { // 오른쪽 확인
+					if(arr[end] >= max_length) { // 오른쪽 중 가장 가까운 max_length 길이의 벽을 찾았다면
+						int max = arr[start] < arr[end] ? arr[start] : arr[end]; // 왼쪽 벽과 오른쪽 벽 중 더 작은 벽을 기준으로
+						for (int i = start + 1; i < end; i++) { // 고인 물의 크기를 더해준다.
 							result += max - arr[i];
 						}
-						break w1;
+						break w1; // 최대 높이를 구해 고인물의 크기를 모두 구했으므로 while문 탈출
 					}
 				}
-			max_length--;
+			max_length--; // 가능한 최대 높이를 낮춰가며 오른쪽 중 가장 높은 크기의 벽을 찾는다.
 			}
 			
-			start = end;
+			start = end; // 현재 구간의 고인물 크기를 모두 구했으므로, 현재 오른쪽 벽을 왼쪽벽으로 하여 다시 오른쪽 구간을 구한다.
 		}
 		
 		System.out.println(result);
