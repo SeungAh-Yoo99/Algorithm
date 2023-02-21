ff28189de0d7969be5c45aae6343c27925e1a060
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2023-02-21 16:18:56 +0900

    Update 빵집.java

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -10,27 +10,27 @@ public class Main {
 	
 	private static boolean pipe(int x, int y) {
 		
-		graph[x][y] = '=';
+		graph[x][y] = '='; // 현재 위치는 파이프 설치 가능한 위치이므로 파이프 설치
 		
-		if(y == C - 1) {
-			return true;
+		if(y == C - 1) { // 마지막 열까지 왔다면 파이프 설치 성공한 것이므로
+			return true; // true 리턴
 		}
 		
 		boolean check = false;
-		if(x - 1 >= 0 && graph[x - 1][y + 1] == '.') {
-			check = pipe(x - 1, y + 1);
+		if(x - 1 >= 0 && graph[x - 1][y + 1] == '.') { // 오른쪽 위 대각선에 파이프 놓을 수 있는 위치면
+			check = pipe(x - 1, y + 1); // 오른쪽 위 대각선 위치로 재귀
 		}
 		
-		if(!check && graph[x][y + 1] == '.') {
-			check = pipe(x, y + 1);
+		if(!check && graph[x][y + 1] == '.') { // 오른쪽 위치에 파이프 놓을 수 있다면
+			check = pipe(x, y + 1); // 오른쪽 위치로 재귀
 		}
 		
-		if(!check && x + 1 < R && graph[x + 1][y + 1] == '.') {
-			check = pipe(x + 1, y + 1);
+		if(!check && x + 1 < R && graph[x + 1][y + 1] == '.') { // 오른쪽 아래 대각선 위치로 파이프 놓을 수 있다면
+			check = pipe(x + 1, y + 1); // 오른쪽 아래 대각선 위치로 재귀
 		}
 		
-		if(!check) graph[x][y] = 'x'; // 현재 위치로는 파이프 설치 불가
-		return check;
+		if(!check) graph[x][y] = 'x'; // 위 조건을 모두 만족하지 못하므로 현재 위치에는 파이프 설치 불가
+		return check; // 설치 여부 리턴
 	}
 	
 	public static void main(String[] args) throws Exception{
@@ -49,10 +49,12 @@ public class Main {
 			graph[i] = br.readLine().toCharArray();
 		}
 		
+		// 0 ~ R - 1 행부터 시작
 		for (int i = 0; i < R; i++) {
 			pipe(i, 0);
 		}
 		
+		// 마지막 열의 파이프 개수 확인
 		int result = 0;
 		for (int i = 0; i < R; i++) {
 			if(graph[i][C - 1] == '=') result++;
