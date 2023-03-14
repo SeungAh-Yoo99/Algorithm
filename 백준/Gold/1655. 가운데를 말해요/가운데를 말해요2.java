5711c1a1dd80dbdacc37fbde53b144c97d903b82
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2023-03-15 00:36:52 +0900

    Create 가운데를 말해요2.java

---
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요2.java
@@ -0,0 +1,43 @@
+// 31452KB	840ms
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] negative = new int[10001]; // -1 ~ -10000이 몇 개 있는지 나타내는 배열
+		int[] positive = new int[10001]; // 0 ~ 10000이 몇 개 있는지 나타내는 배열
+		
+for1:		for (int i = 0; i < N; i++) {
+			int tmp = Integer.parseInt(br.readLine());
+			if(tmp >= 0) positive[tmp]++;
+			else negative[-tmp]++;
+			
+			int count = -1;
+			for (int j = 10000; j > 0; j--) { // 음수값부터
+				count += negative[j];
+				if(count >= i / 2) { // 가운데 값 찾았다면
+					sb.append(-j + "\n");
+					continue for1;
+				}
+			}
+			for (int j = 0; j <= 10000; j++) { // 양수값 확인
+				count += positive[j];
+				if(count >= i / 2) { // 가운데 값 찾았다면
+					sb.append(j + "\n");
+					continue for1;
+				}
+			}
+		}
+		
+		System.out.println(sb);
+	}
+
+}
