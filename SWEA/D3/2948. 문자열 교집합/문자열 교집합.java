5dec262efa6aa7a2ef4fe04cebb6811b4f56e16c
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2023-05-03 23:59:45 +0900

    [D3] Title: 문자열 교집합, Time: 745 ms, Memory: 135,104 KB -BaekjoonHub

--- SWEA/D3/2948. 문자열 교집합/문자열 교집합.java
+++ SWEA/D3/2948. 문자열 교집합/문자열 교집합.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Solution {
@@ -17,18 +17,18 @@ public class Solution {
 			int N = Integer.parseInt(st.nextToken());
 			int M = Integer.parseInt(st.nextToken());
 			
-			// 첫 번째 집합의 원소 문자열 입력 후 HashMap에 저장
-			HashMap<String, Integer> hm = new HashMap<>();
+			// 첫 번째 집합의 원소 문자열 입력 후 HashSet에 저장
+			HashSet<String> hs = new HashSet<>();
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++) {
-				hm.put(st.nextToken(), 0);
+				hs.add(st.nextToken());
 			}
 			
 			// 두 번째 집합의 원소 문자열 입력 후 이미 값이 있다면 count
 			int count = 0;
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < M; i++) {
-				if(hm.containsKey(st.nextToken())) count++;
+				if(hs.contains(st.nextToken())) count++;
 			}
 			
 			// 출력 담기
