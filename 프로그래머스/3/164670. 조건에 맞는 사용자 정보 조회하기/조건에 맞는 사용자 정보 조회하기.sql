0910bd12e0f0793166c5029e389775111701f7f1
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2023-11-27 16:57:01 +0900

    Update 조건에 맞는 사용자 정보 조회하기.sql

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,9 +1,9 @@
-SELECT DISTINCT ugu.user_id, ugu.nickname, CONCAT(ugu.city, ' ', street_address1, ' ', street_address2) AS 전체주소, CONCAT(SUBSTRING(ugu.tlno, 1, 3), '-', SUBSTRING(ugu.tlno, 4, 4), '-', SUBSTRING(ugu.tlno, 8, 4)) AS 전화번호
-FROM used_goods_user AS ugu
-WHERE ugu.user_id in (
+SELECT user_id, nickname, CONCAT(city, ' ', street_address1, ' ', street_address2) AS 전체주소, CONCAT(SUBSTRING(tlno, 1, 3), '-', SUBSTRING(tlno, 4, 4), '-', SUBSTRING(tlno, 8, 4)) AS 전화번호
+FROM used_goods_user
+WHERE user_id IN (
     SELECT writer_id
     FROM used_goods_board
     GROUP BY writer_id
     HAVING COUNT(writer_id) >= 3
 )
-ORDER BY ugu.user_id DESC
+ORDER BY user_id DESC
