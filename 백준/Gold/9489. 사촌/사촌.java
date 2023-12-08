8df2ae03c3caddc6499eabde86d44ccf95afd8df
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2023-12-08 17:03:31 +0900

    [Gold IV] Title: 사촌, Time: 1280 ms, Memory: 167732 KB -BaekjoonHub

--- 백준/Gold/9489. 사촌/사촌.java
+++ 백준/Gold/9489. 사촌/사촌.java
@@ -4,66 +4,60 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception{
+    public static void main(String[] args)  throws Exception{
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		int n;
-		int k;
-		int[][] arr;
-		int kIdx = 0; // k의 인덱스
-		
-		
-		while(true) {
-			// n, k 입력
-			st = new StringTokenizer(br.readLine());
-			n = Integer.parseInt(st.nextToken());
-			k = Integer.parseInt(st.nextToken());
-			
-			// 테스트 케이스 종료
-			if(n == 0 && k == 0) break;
-			
-			// 필요 자료구조 선언
-			arr = new int[n][2];
-			// arr[i][0] := i번째 수
-			// arr[i][1] := i번째 수의 부모 인덱스
-			
-			// 수열 입력
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < n; i++) {
-				arr[i][0] = Integer.parseInt(st.nextToken());
-				if(arr[i][0] == k) kIdx = i;
-			}
-			arr[0][1] = -1; // 루트 노드라고 표시
-			
-			// 두 번재 노드부터 depth 정보 저장
-			int p = 0; // 부모 노드의 인덱스
-			for (int i = 1; i < n; i++) {
-				arr[i][1] = p;
-				
-				// 다음 수가 연속된 수가 아니라면
-				if(i < n - 1 && arr[i][0] + 1 != arr[i + 1][0]) p++;
-			}
-			
-			int result = 0;
-			if(kIdx != 0) {
-				int pp = arr[arr[kIdx][1]][1]; // k의 부모의 부모 인덱스
-				p = arr[kIdx][1]; // k의 부모 인덱스
-				// k 부모의 형제 노드들의 자식 노드 개수 세기
-				for (int i = 1; i < n; i++) {
-					if(arr[i][1] >= kIdx) break; // k의 자식들이 나왔다면 그만 봐도 됨
-					
-					if(arr[i][1] == -1 || arr[arr[i][1]][1] == -1) continue;
-					else if(arr[arr[i][1]][1] == pp && arr[i][1] != p) result++;
-				}
-			}
-			sb.append(result).append("\n");
-		}
-		
-		// 출력
-		System.out.println(sb);
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder result = new StringBuilder(); // 답 저장 문자열
 
+        while(true) { // 0 0이 입력으로 들어오기 전까지 반복
+
+            // n := 노드의 수, k := 사촌의 수
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            if(n == 0 && k == 0) break;
+
+            int[] node = new int[n]; // 노드 정보
+            int[] parent = new int[n]; // 부모 노드 인덱스 정보
+            int kIdx = 0; // k의 인덱스
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                node[i] = Integer.parseInt(st.nextToken());
+                if(node[i] == k) kIdx = i;
+            }
+
+            // node[1]부터 부모 노드 인덱스 구하기
+            int p = -1;
+            parent[0] = -1;
+            for (int i = 1; i < n; i++) {
+                if(node[i] - 1 == node[i - 1]) { // 전 노드와 부모가 같은 경우
+                    parent[i] = p;
+                }
+                else {
+                    parent[i] = ++p;
+                }
+            }
+
+            // kIdx의 형제 노드 개수 구하기
+            int pIdx = parent[kIdx]; // kIdx의 부모 인덱스
+            if(pIdx == -1) { // 루트 노드를 구하는 경우엔 바로 구해줌
+                result.append(0 + "\n");
+                continue;
+            }
+            int ppIdx = parent[pIdx]; // kIdx의 조부모 인덱스
+            int count = 0;
+            for (int i = 1; i < n; i++) {
+                // kIdx와 조부모 인덱스는 같지만 부모 인덱스는 다른 경우만 구하기
+                if(parent[parent[i]] == ppIdx && parent[i] != pIdx) count++;
+            }
+
+            result.append(count + "\n");
+        }
+
+        // 출력
+        System.out.println(result);
+    }
 }
