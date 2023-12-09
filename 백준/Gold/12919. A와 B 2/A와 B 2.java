8484d263bdf73b2e23d49f8a7c9d944a3864ba9e
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2023-12-10 00:53:12 +0900

    [Gold V] Title: A와 B 2, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -5,43 +5,44 @@ import java.util.Queue;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception{
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringBuilder S = new StringBuilder(br.readLine());
-		StringBuilder T = new StringBuilder(br.readLine());
-		
-		int result = 0;
-		Queue<StringBuilder> q = new LinkedList<>();
-		q.add(T);
-		
-		while(!q.isEmpty()) {
-			StringBuilder sb1 = q.poll();
-			StringBuilder sb2 = new StringBuilder(sb1);
-			
-			char[] c1 = sb1.toString().toCharArray();
-			if(c1[c1.length - 1] == 'A') {
-				sb1.delete(c1.length - 1, c1.length);
-				if(sb1.length() == S.length() && sb1.toString().equals(S.toString())) {
-					result = 1;
-					break;
-				}
-				else if(sb1.length() > S.length()) q.add(sb1);
-			}
-			
-			char[] c2 = sb2.toString().toCharArray();
-			if(c1[0] == 'B') {
-				sb2.reverse().delete(c2.length - 1, c2.length);
-				if(sb2.length() == S.length() && sb2.toString().equals(S.toString())) {
-					result = 1;
-					break;
-				}
-				else if(sb2.length() > S.length()) q.add(sb2);
-			}
-		}
-		
-		System.out.println(result);
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        // S, T 입력
+        String S = br.readLine();
+        String T = br.readLine();
+
+        // T에서 S를 만들 수 있는지 확인
+        int result = 0;
+
+        Queue<String> q = new LinkedList<>();
+        q.add(T);
+
+        while(!q.isEmpty()) {
+
+            // 문자열 하나 꺼내기
+            String string = q.poll();
+
+            if(string.equals(S)) {
+                result = 1;
+                break;
+            }
+            
+            if(string.length() == 1) continue;
+
+            // 마지막 문자가 A라면 A를 떼서 큐에 넣기
+            char[] tmp = string.toCharArray();
+            StringBuilder tmpSb = new StringBuilder(string);
+            if(tmp[string.length() - 1] == 'A') {
+                q.add(tmpSb.substring(0, string.length() - 1));
+            }
+            // 첫 문자가 B라면 뒤집고 B 떼서 큐에 넣기
+            if(tmp[0] == 'B') {
+                q.add(tmpSb.reverse().substring(0, string.length() - 1));
+            }
+        }
+
+        System.out.println(result);
+    }
 }
