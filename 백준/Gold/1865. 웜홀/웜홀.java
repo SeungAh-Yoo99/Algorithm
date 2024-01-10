8cbff0e85ea9fae6b6a2a7afd0ed21c1e1c4a628
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2024-01-11 00:08:29 +0900

    [Gold III] Title: 웜홀, Time: 1164 ms, Memory: 267488 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -1,66 +1,94 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+    static int N;
+    static ArrayList<ArrayList<int[]>> edges;
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		final int MAX_VALUE = 5_000_001;
-		
-		int TC = Integer.parseInt(br.readLine());
-for1:	for (int tc = 0; tc < TC; tc++) {
-			// 입력
-			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-			int W = Integer.parseInt(st.nextToken());
-			
-			int[][] arr = new int[N + 1][N + 1];
-			for (int i = 1; i <= N; i++) {
-				Arrays.fill(arr[i], MAX_VALUE);
-			}
-			
-			for (int i = 0; i < M; i++) {
-				st = new StringTokenizer(br.readLine());
-				int S = Integer.parseInt(st.nextToken());
-				int E = Integer.parseInt(st.nextToken());
-				int T = Integer.parseInt(st.nextToken());
-				arr[S][E] = Math.min(arr[S][E], T);
-				arr[E][S] = Math.min(arr[E][S], T);
-			}
-			for (int i = 0; i < W; i++) {
-				st = new StringTokenizer(br.readLine());
-				int S = Integer.parseInt(st.nextToken());
-				int E = Integer.parseInt(st.nextToken());
-				int T = Integer.parseInt(st.nextToken());
-				arr[S][E] = Math.min(arr[S][E], -T);;
-			}
-			
-			// 출발
-			for (int i = 1; i <= N; i++) {
-				for (int j = 1; j <= N; j++) {
-					for (int k = 1; k <= N; k++) {
-						arr[j][k] = Math.min(arr[j][k], arr[j][i] + arr[i][k]);
-					}
-				}
-			}
-			
-			for (int i = 1; i <= N; i++) {
-				if(arr[i][i] < 0) {
-					sb.append("YES\n");
-					continue for1;
-				}
-			}
-			sb.append("NO\n");
-		}
+    public static void main(String[] args) throws Exception{
 
-		// 출력
-		System.out.println(sb);
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder answer = new StringBuilder();
+
+        // TC := 테스트케이스의 개수
+        int TC = Integer.parseInt(br.readLine());
+
+        int M, W, S, E, T;
+        for (int tc = 0; tc < TC; tc++) {
+            // N := 지점의 수, M := 도로의 개수, W := 웜홀의 개수
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
+
+            // edges := 간선 정보
+            edges = new ArrayList<>();
+            for (int i = 0; i <= N; i++) {
+                edges.add(new ArrayList<>());
+            }
+
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                S = Integer.parseInt(st.nextToken());
+                E = Integer.parseInt(st.nextToken());
+                T = Integer.parseInt(st.nextToken());
+                edges.get(S).add(new int[] {E, T});
+                edges.get(E).add(new int[] {S, T});
+            }
+
+            for (int i = 0; i < W; i++) {
+                st = new StringTokenizer(br.readLine());
+                S = Integer.parseInt(st.nextToken());
+                E = Integer.parseInt(st.nextToken());
+                T = Integer.parseInt(st.nextToken());
+                edges.get(S).add(new int[] {E, T * -1});
+            }
+
+            // 벨만포드 알고리즘을 사용하여 음수 사이클이 생겼는지 확인
+            if(isPossible()) answer.append("YES\n");
+            else answer.append("NO\n");
+        }
+
+        System.out.println(answer);
+    }
+
+    private static boolean isPossible() { // 벨만포드 알고리즘을 사용하여 음수 사이클이 생겼는지 확인
+
+        // i부터 다른 지점에 도착하는 경우 구하기
+        int[] minDis, now, next; Queue<int[]> q; ArrayList<int[]> edge;
+        for (int i = 1; i <= N; i++) {
+
+            minDis = new int[N + 1];
+            Arrays.fill(minDis, Integer.MAX_VALUE);
+            minDis[i] = 0;
+
+            q = new LinkedList<>();
+            q.add(new int[] {i, 0, 0});
+
+            while(!q.isEmpty()) {
+                now = q.poll();
+
+                edge = edges.get(now[0]);
+                for (int j = 0; j < edge.size(); j++) {
+                    next = edge.get(j);
+
+                    if(minDis[next[0]] > now[1] + next[1]) { // 더 짧은 경로를 찾았을 때만 이동
+                        if(now[2] + 1 == N) { // 벨만포드 알고리즘에 의해서 N - 1개 이상의 간선을 지났다면 음수 사이클이 생긴 것
+                            return true;
+                        }
+                        minDis[next[0]] = now[1] + next[1];
+
+                        q.add(new int[] {next[0], now[1] + next[1], now[2] + 1});
+                    }
+                }
+            }
+        }
+
+        // 모든 경우를 통과하고 여기까지 왔다면 음수 사이클이 생기지 않은 것
+        return false;
+    }
 
 }
