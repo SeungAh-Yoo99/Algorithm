a6821afb7e62345f36deb86731ec9e91cf059caa
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2024-01-13 16:43:53 +0900

    Update NULL 처리하기.sql

--- 프로그래머스/Lv.2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/Lv.2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,3 +1,10 @@
-SELECT animal_type, COALESCE(name, 'No name'),sex_upon_intake
+SELECT animal_type, COALESCE(name, 'No name'), sex_upon_intake
+FROM animal_ins
+ORDER BY animal_id
+
+SELECT
+    animal_type,
+    CASE WHEN name IS NULL THEN 'No name' ELSE name END,
+    sex_upon_intake
 FROM animal_ins
 ORDER BY animal_id
