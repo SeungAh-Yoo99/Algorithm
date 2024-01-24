57670e7776617f4d87760e9553ee1cc6d230843c
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2024-01-24 19:36:55 +0900

    [Gold III] Title: ACM Craft, Time: 1100 ms, Memory: 229660 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -6,74 +6,79 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N;
-	static int[] cost;
-	static ArrayList<ArrayList<Integer>> list;
-	
-	private static int bfs(int start) {
-		
-		int[] visited = new int[N + 1];
-		visited[start] = cost[start];
-		
-		Queue<Integer> q = new LinkedList<>();
-		q.add(start);
-		
-		int result = cost[start];
-		
-		while(!q.isEmpty()) {
-			int now = q.poll();
-			
-			ArrayList<Integer> al = list.get(now);
-			for (int i = 0; i < al.size(); i++) {
-				int next = al.get(i);
-				if(visited[next] < visited[now] + cost[next]) {
-					visited[next] = visited[now] + cost[next];
-					result = Math.max(result, visited[next]);
-					q.add(next);
-				}
-			}
-		}
-		
-		return result;
-	}
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		for (int t = 0; t < T; t++) {
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			int K = Integer.parseInt(st.nextToken());
-			
-			cost = new int[N + 1];
-			st = new StringTokenizer(br.readLine());
-			for (int i = 1; i <= N; i++) {
-				cost[i] = Integer.parseInt(st.nextToken());
-			}
-			
-			list = new ArrayList<>();
-			for (int i = 0; i <= N; i++) {
-				list.add(new ArrayList<>());
-			}
-			
-			for (int i = 0; i < K; i++) {
-				st = new StringTokenizer(br.readLine());
-				int X = Integer.parseInt(st.nextToken());
-				int Y = Integer.parseInt(st.nextToken());
-				list.get(Y).add(X);
-			}
-			
-			int W = Integer.parseInt(br.readLine());
-			
-			sb.append(bfs(W) + "\n");
-		}
-		
-		System.out.println(sb);
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder answer = new StringBuilder();
 
+        // T := 테스트케이스의 개수
+        int T = Integer.parseInt(br.readLine());
+
+        int N, K, X, Y, W, next; int[] D; ArrayList<ArrayList<Integer>> edges; long[] time;
+        Queue<long[]> q; long[] now; ArrayList<Integer> edge;
+        for (int tc = 0; tc < T; tc++) {
+            // N := 건물의 개수, K := 건설순서 규칙 개수
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+
+            // 건설에 걸리는 시간
+            D = new int[N + 1];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= N; i++) {
+                D[i] = Integer.parseInt(st.nextToken());
+            }
+
+            // 건설 규칙 (i번 건물을 짓기 위해 먼저 지어야 하는 건물을 담음)
+            edges = new ArrayList<>();
+            for (int i = 0; i <= N; i++) {
+                edges.add(new ArrayList<>());
+            }
+
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                X = Integer.parseInt(st.nextToken());
+                Y = Integer.parseInt(st.nextToken());
+                edges.get(Y).add(X);
+            }
+
+            // W := 건설해야 할 건물의 번호
+            W = Integer.parseInt(br.readLine());
+
+            // W부터 시작해서 거꾸로 확인하여 가장 건물 짓는 시간 확인
+            time = new long[N + 1];
+            time[W] = D[W];
+
+            q = new LinkedList<>();
+            q.add(new long[] {W, D[W]});
+
+            while(!q.isEmpty()) {
+                now = q.poll();
+
+                if(time[(int)now[0]] > now[1]) continue;
+
+                edge = edges.get((int)now[0]);
+
+                for (int i = 0; i < edge.size(); i++) {
+                    next = edge.get(i);
+
+                    if(time[next] < now[1] + D[next]) {
+                        time[next] = now[1] + D[next];
+                        q.add(new long[] {next, time[next]});
+                    }
+                }
+            }
+
+            long result = 0;
+            for (int i = 1; i <= N; i++) {
+                result = Math.max(result, time[i]);
+            }
+
+            answer.append(result + "\n");
+        }
+
+        System.out.println(answer);
+    }
 }
