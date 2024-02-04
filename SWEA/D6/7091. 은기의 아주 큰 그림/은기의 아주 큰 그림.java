3ba33d1e4fa630d4eb66aff365b9163e8e6ab003
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2024-02-04 23:18:59 +0900

    Update 은기의 아주 큰 그림.java

--- SWEA/D6/7091. 은기의 아주 큰 그림/은기의 아주 큰 그림.java
+++ SWEA/D6/7091. 은기의 아주 큰 그림/은기의 아주 큰 그림.java
@@ -54,6 +54,7 @@ public class Solution {
                 dreamHash = dreamHash.multiply(MOD2).add(row);
             }
 
+			// 왼쪽부터 W 길이의 각 줄의 가로 해시를 구해 hash 배열에 저장
             BigInteger[] hash = new BigInteger[N];
             for (int i = 0; i < N; i++) {
                 hash[i] = new BigInteger("0");
@@ -63,22 +64,29 @@ public class Solution {
                 }
             }
 
-            subMul1 = MOD1.pow(W - 1);
-            subMul2 = MOD2.pow(H - 1);
+            subMul1 = MOD1.pow(W - 1); // 가로 해시의 맨 앞 값을 빼기 위해 미리 계산해둔 값(2^{W-1})
+            subMul2 = MOD2.pow(H - 1); // 세로 해시의 맨 앞 값을 빼기 위해 미리 계산해둔 값(3^{H-1})
 
             result = 0;
-            for (int i = 0; i < M - W + 1; i++) {
+            for (int i = 0; i < M - W + 1; i++) { // 맨 왼쪽부터 오른쪽으로 이동하며 확인
+            
+            	// 처음엔 위에서 구해둔 각 줄의 가로 해시를
+                // 맨 위에서부터 H 길이의 세로 해시를 구한다.
                 tmp = new BigInteger("0");
                 for (int j = 0; j < H; j++) {
                     tmp = tmp.multiply(MOD2).add(hash[j]);
                 }
+                // 은기 꿈그림 해시와 같다면 답 증가시켜준다.
                 if(dreamHash.compareTo(tmp) == 0) result++;
+                
+                // 한 줄씩 밑으로 내려가면서 맨 앞의 값은 빼주고 한 줄을 새로 더해준다.
                 for (int j = 0; j < N - H; j++) {
                     tmp = tmp.subtract(hash[j].multiply(subMul2)).multiply(MOD2).add(hash[j + H]);
-                    if(dreamHash.compareTo(tmp) == 0) result++;
+                    if(dreamHash.compareTo(tmp) == 0) result++; // 은기 꿈그림 해시와 비교
                 }
 
                 if(i != M - W) {
+                	// 가로 해시를 구해놓은 hash 배열도 한 줄씩 오른쪽이로 이동하며, 맨 앞의 값은 빼주고 한 줄을 새로 더해준다.
                     for (int j = 0; j < N; j++) {
                         if(full[j][i] == 'o') hash[j] = hash[j].subtract(subMul1);
                         hash[j] = hash[j].multiply(MOD1);
