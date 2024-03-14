31b19e0473ea33e919bf4326a67645f6405dc944
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2024-03-14 12:45:06 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,10 @@
+SELECT
+    animal_id,
+    name,
+    case
+        when sex_upon_intake like 'Neutered%' then 'O'
+        when sex_upon_intake like 'Spayed%' then 'O'
+        else 'X'
+    end as 중성화
+FROM animal_ins
+ORDER BY animal_id
