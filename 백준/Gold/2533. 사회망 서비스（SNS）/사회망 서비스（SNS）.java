1a2fe7b6f8b56c5fbdc866df8cfc78789436964e
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2024-03-16 17:05:44 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 2016 ms, Memory: 404356 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -4,67 +4,64 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static ArrayList<ArrayList<Integer>> list;
-	static boolean[] visited;
-	static int[][] dp;
-	static int result;
-	
-	private static void getAA(int node) {
-		/*
-		 * 현재 노드가 얼리 아답터가 아니면
-		 * 모든 자식 노드가 얼리 아답터이어야 하므로
-		 * 자식 노드가 얼리 아답터인 경우를 더해줌
-		 * 
-		 * 현재 노드가 얼리 아답터라면
-		 * 자식 노드는 얼리 아답터거나 아니거나 상관 없으므로
-		 * 둘 중 더 작은 수를 더해줌
-		*/
-		
-		dp[node][0] = 0;
-		dp[node][1] = 1;
-		visited[node] = true;
-		
-		ArrayList<Integer> tmpList = list.get(node); // node의 자녀 노드 리스트를 가져옴
-		
-		for (int i = 0; i < tmpList.size(); i++) {
-			int child = tmpList.get(i);
-			if(!visited[child]) {
-				getAA(child); // 자식 노드 dfs
-				dp[node][0] += dp[child][1];
-				dp[node][1] += Math.min(dp[child][0], dp[child][1]);
-			}
-		}
-	}
 
-	public static void main(String[] args) throws Exception{
+    static ArrayList<ArrayList<Integer>> edges;
+    static boolean[] visited;
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int N = Integer.parseInt(br.readLine()); // 친구 관계 트리의 정점 개수
-		list = new ArrayList<>();
-		for (int i = 0; i <= N; i++) {
-			list.add(new ArrayList<>());
-		}
-		 
-		for (int i = 1; i < N; i++) { // N개의 정점 입력
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			list.get(a).add(b);
-			list.get(b).add(a);
-		}
-		 
-		// dp[i][0]은 i노드가 얼리아답터가 아닌 경우의 수
-		// dp[i][1]은 i노드가 얼리아답터일 경우의 수
-		dp = new int[N + 1][2];
-		visited = new boolean[N + 1];
-		
-		// dfs
-		getAA(1);
-		 
-		System.out.println(Math.min(dp[1][0], dp[1][1]));
-	}
+    public static void main(String[] args) throws Exception{
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        edges = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            edges.add(new ArrayList<>());
+        }
+
+        int u, v;
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            u = Integer.parseInt(st.nextToken());
+            v = Integer.parseInt(st.nextToken());
+
+            edges.get(u).add(v);
+            edges.get(v).add(u);
+        }
+
+        visited = new boolean[N + 1];
+
+        int[] ret = dfs(1);
+        System.out.println(Math.min(ret[0], ret[1]));
+    }
+
+    private static int[] dfs(int node) {
+
+        // ret[0] := node가 얼리 아답터가 아닌 경우
+        // ret[1] := node가 얼리 아답터인 경우
+        int[] ret = {0, 1};
+
+        visited[node] = true;
+
+        ArrayList<Integer> edge = edges.get(node);
+
+        int next;
+        int[] tmp;
+        for (int i = 0; i < edge.size(); i++) {
+            next = edge.get(i);
+
+            if(!visited[next]) {
+                tmp = dfs(next);
+
+                // node가 얼리 아답터가 아닌 경우, node에 연결된 다른 노드들은 모두 얼리 아답터여야 한다.
+                ret[0] += tmp[1];
+
+                // node가 얼리 아답터인 경우, node에 연결된 다른 노드들은 얼리 아답터거나 아니거나 상관없다.
+                ret[1] += Math.min(tmp[0], tmp[1]);
+            }
+        }
+
+        return ret;
+    }
 }
