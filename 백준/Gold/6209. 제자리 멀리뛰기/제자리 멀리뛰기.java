1372a9d1c984b4d58e14d84b10989eae56f8f72f
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2024-05-28 15:34:55 +0900

    [Gold III] Title: 제자리 멀리뛰기, Time: 308 ms, Memory: 26348 KB -BaekjoonHub

---
+++ 백준/Gold/6209. 제자리 멀리뛰기/제자리 멀리뛰기.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int d = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] len = new int[n + 2];
+        for (int i = 0; i < n; i++) {
+            len[i] = Integer.parseInt(br.readLine());
+        }
+        len[n] = 0;
+        len[n + 1] = d;
+
+        Arrays.sort(len);
+
+        // 이분 탐색
+        int start = 0, end = d, mid;
+        int count, left, tmp;
+        int result = 0;
+        while(start <= end) {
+            mid = (start + end) / 2;
+
+           count = 0; left = 0; tmp = 1;
+           while(left < n + 1) {
+
+               if(tmp < n + 2 && len[tmp] - len[left] < mid) tmp++;
+               else {
+                   count += tmp - left - 1;
+                   left = tmp++;
+               }
+           }
+
+           if(count <= m) {
+               result = mid;
+               start = mid + 1;
+           }
+           else {
+               end = mid - 1;
+           }
+        }
+
+        System.out.println(result);
+    }
+}
