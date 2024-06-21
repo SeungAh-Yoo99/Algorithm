1b832fc48fb2f14d55849a4df8e142c46f78fe6f
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2024-06-21 20:52:22 +0900

    Update 파일 합치기 3.java

--- 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.java
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.java
@@ -20,6 +20,10 @@ public class Main {
         for (int tc = 0; tc < T; tc++) {
             K = Integer.parseInt(br.readLine());
 
+            // 큰 비용이 드는 파일일수록 되도록 합치는 연산을 하지 않는 것이 좋다.
+            // 우선순위 큐에 비용을 넣어두고
+            // 계속 적은 비용이 드는 것들끼리만 합쳐준다.
+
             pq = new PriorityQueue<>();
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < K; i++) {
@@ -27,11 +31,11 @@ public class Main {
             }
 
             cost = 0;
-            while(pq.size() > 1) {
+            while(pq.size() > 1) { // 장이 하나가 될 때까지
                 a = pq.poll();
-                b = pq.poll();
-                cost += a + b;
-                pq.add(a + b);
+                b = pq.poll(); // 가장 비용이 적은 값 두 개를 꺼내
+                cost += a + b; // 비용을 추가해주고
+                pq.add(a + b); // 다시 큐에 넣는다
             }
 
             result.append(cost + "\n");
