3e26d95b8442bf8e39b86ae293442be78e24942c
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2024-07-24 22:26:43 +0900

    Update 원자의 에너지.java

--- 백준/Gold/2058. 원자의 에너지/원자의 에너지.java
+++ 백준/Gold/2058. 원자의 에너지/원자의 에너지.java
@@ -9,8 +9,6 @@ import java.util.StringTokenizer;
  *
  * N개의 에너지 상태가 주어지므로, 노드는 N개
  * 만약 노드 A에서 양성자 중 하나를 더하거나 빼서 노드 B값이 나오면 두 노드는 간선으로 연결된 것(A->B)
- * 모든 노드들이 하나의 트리로 연결된 것은 아님.
- * 여러 개의 트리에 나눠 연결되어 있을 수 있다.
  *
  * 간선으로 연결된 두 개의 노드는 함께 컨테이너에 집어넣을 수 없음
  * DP를 통해 현재 노드를 넣거나 뺀 경우 중 더 나은 경우를 구해야함
@@ -18,9 +16,9 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int M;
-    static Set<Integer> state;
+    static boolean[] state;
     static int[] energy;
-    static Set<Integer> visited;
+    static boolean[] visited;
 
     public static void main(String[] args) throws Exception{
 
@@ -31,13 +29,15 @@ public class Main {
         M = Integer.parseInt(st.nextToken()); // 양성자의 개수
 
         // 원자가 가질 수 있는 에너지 상태
-        state = new HashSet<>();
-        int start = 0, n;
+        state = new boolean[1_000_001];
+        // 임의로 정한 루트 노드의 값
+        int start = 0;
+        int n;
         for (int i = 0; i < N; i++) {
             n = Integer.parseInt(br.readLine());
 
             if(i == 0) start = n;
-            state.add(n);
+            state[n] = true;
         }
 
         // 양성자가 가진 에너지
@@ -47,34 +47,35 @@ public class Main {
         }
 
         // 노드 방문 체크
-        visited = new HashSet<>();
-
-        // result[0] := now을 포함했을 경우의 최대값
-        // result[1] := now을 포함하지 않았을 경우의 최대값
+        visited = new boolean[1_000_001];
+        
         int[] result = dfs(start);
         System.out.println(Math.max(result[0], result[1]));
     }
 
-    private static int[] dfs(int n) { // n := 현재 상태
+    private static int[] dfs(int now) { // now := 현재 상태
 
         // a -> b -> c -> b -> a와 같은 순환구조를 막기 위해 방문체크
-        visited.add(n);
+        visited[now] = true;
 
-        // result[0] := n을 포함했을 경우의 최대값
-        // result[1] := n을 포함하지 않았을 경우의 최대값
+        // result[0] := now를 포함했을 경우의 최대값
+        // result[1] := now를 포함하지 않았을 경우의 최대값
         int[] result = new int[2];
-        result[0] = n;
+        result[0] = now;
 
         int[] tmp;
         for (int i = 0; i < M; i++) {
             // 양성자를 받아들이거나 내쏘아서 만들 수 있는 새로운 원자
-            for(int newState : new int[] {n + energy[i], n - energy[i]}) {
-                if(newState > 0) {
-                    if(state.contains(newState) && !visited.contains(newState)) { // 만들 수 있는 상태라면
+            for(int newState : new int[] {now + energy[i], now - energy[i]}) {
+                if(newState > 0 && newState < 1_000_001) {
+                    if(state[newState] && !visited[newState]) { // 만들 수 있는 상태라면(연결된 노드를 찾았다면)
+                        // now의 자식노드인 newState의 최대값부터 구하러감
                         tmp = dfs(newState);
-                        // newState들을 포함하지 않았을 경우의 최대값
+                        // result[0]은 now를 포함했을 경우의 최댓값
+                        // now와 연결된 newState를 포함하지 않은 경우만 올 수 있음
                         result[0] += tmp[1];
-                        // newState를 포함하거나 포함하지 않았을 때 중 최대값
+                        // result[1]은 now를 포함하지 않았을 경우의 최대값
+                        // now와 연결된 newState를 포함하건 포함하지 않건 상관없음(둘 중 더 큰 값으로)
                         result[1] += Math.max(tmp[0], tmp[1]);
                     }
                 }
