b6c0e2b01f66ec9379c35c956f621223b206a237
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2024-07-26 20:48:39 +0900

    [Gold V] Title: 두 수의 합, Time: 2072 ms, Memory: 231292 KB -BaekjoonHub

--- 백준/Gold/9024. 두 수의 합/두 수의 합.java
+++ 백준/Gold/9024. 두 수의 합/두 수의 합.java
@@ -5,9 +5,6 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int n, K;
-    static int[] arr;
-
     public static void main(String[] args) throws Exception{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,6 +13,8 @@ public class Main {
 
         int t = Integer.parseInt(br.readLine());
 
+        int n, K;
+        int[] arr;
         for (int tc = 0; tc < t; tc++) {
             st = new StringTokenizer(br.readLine());
             n = Integer.parseInt(st.nextToken());
@@ -32,70 +31,30 @@ public class Main {
             Arrays.sort(arr);
 
             int min = Integer.MAX_VALUE;
-            int result =0;
+            int result = 0;
 
-            int[] ret;
-            for (int i = 0; i < n - 1; i++) {
-                // 이분탐색으로
-                ret = getMinSub(i, i + 1, n - 1);
+            int start = 0, end = n - 1;
+            int sub;
+            while(start < end) {
 
-                if(min > ret[0]) {
-                    min = ret[0];
-                    result = ret[1];
+                sub = Math.abs(K - (arr[start] + arr[end]));
+                if(min > sub) {
+                    min = sub;
+                    result = 1;
                 }
-                else if(min == ret[0]) {
-                    result += ret[1];
+                else if (min == sub) {
+                    result++;
                 }
+
+                if(arr[start] + arr[end] < K) {
+                    start++;
+                }
+                else end--;
             }
 
             answer.append(result + "\n");
         }
 
         System.out.print(answer);
-    }
-
-    // start부터 end까지 index와의 합이 K와 가장 가까운 수의 차 구하기
-    private static int[] getMinSub(int index, int start, int end) {
-
-        int[] ret = new int[2];
-        ret[0] = Integer.MAX_VALUE;
-
-        // 찾고자 하는 수
-        int find = K - arr[index];
-
-        int mid, get = start;
-        while(start <= end) {
-            mid = (start + end) / 2;
-
-            if(arr[mid] > find) end = mid - 1;
-            else {
-                get = mid;
-                start = mid + 1;
-            }
-        }
-
-        // 찾은 수
-        int sub = Math.abs(K - (arr[index] + arr[get]));
-        if(ret[0] > sub) {
-            ret[0] = sub;
-            ret[1] = 1;
-        }
-        else if(ret[0] == sub) {
-            ret[1]++;
-        }
-
-        // 찾은 수보다 더 큰 수 중 가장 작은 수도 검사
-        if(get != n - 1) {
-            sub = Math.abs(K - (arr[index] + arr[get + 1]));
-            if(ret[0] > sub) {
-                ret[0] = sub;
-                ret[1] = 1;
-            }
-            else if(ret[0] == sub) {
-                ret[1]++;
-            }
-        }
-
-        return ret;
     }
 }
