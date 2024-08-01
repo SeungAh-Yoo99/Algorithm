2feb09a816971656cb72ad230f998f4a38272f96
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2024-08-01 18:35:14 +0900

    [level 3] Title: 네트워크, Time: 0.45 ms, Memory: 93.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,44 @@
+class Solution {
+    
+    static int[] parent;
+    
+    public int solution(int n, int[][] computers) {
+
+        // 유니온 파인드로 네트워크 개수 구하기
+        parent = new int[n];
+        for(int i = 0; i < n; i++) parent[i] = i;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(computers[i][j] == 1) union(i, j);
+            }
+        }
+        
+        // 네트워크 개수 세기
+        int answer = 0;
+        boolean[] visited = new boolean[n];
+        for(int i = 0; i < n; i++) {
+            if(parent[i] == i && !visited[i]) {
+                answer++;
+                visited[i] = true;
+            }
+        }
+        
+        return answer;
+    }
+    
+    private static int find(int a) {
+        if(parent[a] == a) return a;
+        
+        return parent[a] = find(parent[a]);
+    }
+    
+    private static void union(int a, int b) {
+        
+        a = find(a);
+        b = find(b);
+        
+        if(a < b) parent[b] = a;
+        else parent[a] = b;
+    }
+} 
