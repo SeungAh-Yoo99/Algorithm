b9f79b245b8d5b9874583be14d5c759321111bae
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2024-08-05 17:26:07 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 108.45 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] food_times, long k) {
+        
+        // food_times를 올림차순으로 정렬
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < food_times.length; i++) {
+            pq.add(food_times[i]);
+        }
+        
+        // 몇 번 회전했는지 저장
+        int count = 0;
+        
+        int min;
+        while(!pq.isEmpty() && k >= (long)pq.size()) {
+            // 현재 가장 적게 남은 음식
+            min = pq.peek() - count;
+            
+            // 가장 적게 남은 음식이 끝날 때까지 회전
+            if(k >= (long)min * pq.size()) {
+                count += min;
+                k = k - (long)min * pq.size();
+            }
+            // 가장 적게 남은 음식이 끝날 때까지 회전하진 못한다면
+            else {
+                // 돌 수 있는 만큼은 돌기
+                count += (int)(k / (long)pq.size());
+                k = (long)(k % (long)pq.size());
+            }
+            
+            // 끝난 음식 제거
+            while(!pq.isEmpty() && pq.peek() == count) pq.poll();
+        }
+        
+        // 회전할 수 있는만큼 회전했으므로
+        // 마지막으로 순회하며 먹기 시작하는 음식 구하기
+        int answer = -1;
+        for(int i = 0; i < food_times.length; i++) {
+            if(food_times[i] > count) k--;
+            // 다시 먹기 시작하는 음식 찾음
+            if(k == -1) {
+                answer = i + 1;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
