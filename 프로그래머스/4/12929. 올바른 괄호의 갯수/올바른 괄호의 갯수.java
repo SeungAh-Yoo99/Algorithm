5b06fc3c7c97a752766eb21ac2e1a145083964de
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2024-08-09 13:55:07 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 26.49 ms, Memory: 93.6 MB -BaekjoonHub

---
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
@@ -0,0 +1,36 @@
+//import java.util.*;
+
+class Solution {
+    
+    static int n;
+    static int answer;
+    
+    public int solution(int n) {
+        
+        this.n = n;
+        
+        answer = 0;
+        backTracking(0, 0);
+        return answer;
+    }
+    
+    // open := 열린 괄호가 사용된 개수
+    // close := 닫힌 괄호가 사용된 개수
+    private static void backTracking(int open, int close) {
+        
+        if(open == n && close == n) {
+            answer++;
+            return;
+        }
+        
+        // 올 수 있는 열린 괄호 개수가 남았다면
+        if(open < n) {
+            backTracking(open + 1, close);
+        }
+        
+        // 올 수 있는 닫힌 괄호 개수가 남았다면
+        if(open > close) {
+            backTracking(open, close + 1);
+        }
+    }
+}
