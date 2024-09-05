14ecd0157ab650dcd1034eb06df0f3cb59255b79
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2024-09-05 10:12:28 +0900

    [Silver I] Title: 정수 삼각형, Time: 236 ms, Memory: 26464 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,34 +1,40 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scan=new Scanner(System.in);
-		int n=scan.nextInt();
-		int dp[][]=new int[n+1][n+1];
-		
-		for (int i=1;i<=n;i++)
-			for (int j=1;j<=i;j++)
-				dp[i][j]=scan.nextInt();
-		
-		for (int i=2; i<=n;i++) {
-			dp[i][1]+=dp[i-1][1];
-			for (int j=2;j<i;j++) {
-				if (dp[i-1][j-1]>dp[i-1][j])
-					dp[i][j]+=dp[i-1][j-1];
-				else
-					dp[i][j]+=dp[i-1][j];
-			}
-			dp[i][i]+=dp[i-1][i-1];
-		}
-		
-		int max=0;
-		for (int i=1;i<=n;i++)
-			if (max<dp[n][i])
-				max=dp[n][i];
-		
-		System.out.println(max);
-	}
+    public static void main(String[] args) throws Exception{
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= i; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 삼각형의 왼쪽, 오른쪽에 여백을 줌
+        int[][] dp = new int[n + 1][n + 2];
+
+        // 리프 노드를 dp에 담아줌
+        for (int i = 1; i <= n; i++) {
+            dp[n][i] = arr[n][i];
+        }
+
+        // 자신의 왼쪽, 오른쪽 자식 중 더 큰 수를 가져옴
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                dp[i][j] = Math.max(dp[i + 1][j], dp[i + 1][j + 1]) + arr[i][j];
+            }
+        }
+
+        // 출력
+        System.out.println(dp[1][1]);
+    }
 }
