ac7e787057552f76996747cc085843a8945f7bfe
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2024-10-14 02:59:57 +0900

    [Gold I] Title: 클레어와 물약, Time: 1356 ms, Memory: 148628 KB -BaekjoonHub

--- 백준/Gold/20119. 클레어와 물약/클레어와 물약.java
+++ 백준/Gold/20119. 클레어와 물약/클레어와 물약.java
@@ -16,18 +16,21 @@ public class Main {
 
         // 위상 정렬
 
-        ArrayList<ArrayList<int[]>> edges = new ArrayList<>(); // edges.get(i) := i번 물약을 재료로 가지고 있는 물약 리스트
+        // edges.get(i) := i번 물약을 재료로 가지고 있는 물약 리스트
+        // edges.get(i).get(j) -> {a, b} := a번 물약의 b번 레시피에서 i번 물약을 재료로 사용함
+        ArrayList<ArrayList<int[]>> edges = new ArrayList<>();
         for (int i = 0; i <= N; i++) {
             edges.add(new ArrayList<>());
         }
 
-        ArrayList<ArrayList<Integer>> count = new ArrayList<>(); // count.get(i) := i번 물약을 만들기 위해 필요한 물약 개수
+        // count.get(i) := i번 물약을 만들기 위해 필요한 레시피별 물약 개수
+        ArrayList<ArrayList<Integer>> count = new ArrayList<>();
         for (int i = 0; i <= N; i++) {
             count.add(new ArrayList<>());
         }
 
         Queue<Integer> q = new ArrayDeque<>();
-        int k, r;
+        int k, r, index;
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             k = Integer.parseInt(st.nextToken());
@@ -36,11 +39,13 @@ public class Main {
             }
             r = Integer.parseInt(st.nextToken());
 
-            while(!q.isEmpty()) { // 재료 물약들에게 r 물약의 재료로 사용된다고 알려줌
-                edges.get(q.poll()).add(new int[] {r, count.get(r).size()});
+            // 재료 물약들에게 r번 물약 index번 레시피 재료로 사용된다고 알려줌
+            index = count.get(r).size();
+            while(!q.isEmpty()) {
+                edges.get(q.poll()).add(new int[] {r, index});
             }
 
-            count.get(r).add(k); // r번 물약을 만들기 위해선 k개의 재료 물약이 필요함
+            count.get(r).add(k); // r번 물약의 index번 레시피에 k개의 재료 물약이 필요함
         }
 
         int L = Integer.parseInt(br.readLine());
@@ -59,15 +64,15 @@ public class Main {
         while(!q.isEmpty()) {
             now = q.poll(); // 이미 가지고 있거나, 만들 수 있는 물약
 
-            edge = edges.get(now); // now번 물약을 재료로 사용하는 물약 리스트
+            edge = edges.get(now); // now번 물약을 재료로 사용하는 레시피 리스트
             for (int i = 0; i < edge.size(); i++) {
                 next = edge.get(i);
 
+                // 이미 가지고 있거나 만들 수 있는 물약이면 넘어감
                 if(count.get(next[0]).isEmpty()) continue;
 
-                // 필요한 재료 물약을 모두 가지게 되었다면, 이미 가지고 있는 물약 큐에 넣어줌
-                tmp = count.get(next[0]).get(next[1]);
-                if(tmp == 1) {
+                tmp = count.get(next[0]).get(next[1]); // next[0]번 물약의 next[1]번 레시피에 필요한 재료 물약 개수
+                if(tmp == 1) { // 필요한 재료 물약을 모두 가지게 되었음
                     q.add(next[0]);
                     count.get(next[0]).clear();
                     resultList.add(next[0]);
@@ -80,6 +85,7 @@ public class Main {
 
         Collections.sort(resultList); // 답 리스트 정렬
 
+        // 출력
         StringBuilder result = new StringBuilder();
         result.append(resultList.size()).append("\n");
         for (int i = 0; i < resultList.size(); i++) {
