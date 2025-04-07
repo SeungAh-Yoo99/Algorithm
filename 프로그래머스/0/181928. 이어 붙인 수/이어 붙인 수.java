db90622c5460ccbb70a193794afb477298aec3ed
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2025-04-07 17:26:09 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] num_list) {
+        
+        int odd = 0, even = 0;
+        
+        for(int i = 0; i < num_list.length; i++) {
+            if(num_list[i] % 2 == 0)
+                even = even * 10 + num_list[i];
+            else
+                odd = odd * 10 + num_list[i];
+        }
+        
+        
+        int answer = odd + even;
+        return answer;
+    }
+}
