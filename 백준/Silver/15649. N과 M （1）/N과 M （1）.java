51be0b40363f8f7699503a8e1a7095e63350dc3f
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2025-11-29 19:56:47 +0900

    [Silver III] Title: N과 M (1), Time: 156 ms, Memory: 21568 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,59 +3,45 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int n;
-	static int m;
-	static int[] arr;
-	static boolean[] visited;
-	static StringBuilder sb;
-	
-	public static void progression(int idx, int num) {
-		arr[idx] = num;
-		visited[num] = true;
-		
-		// 마지막 인덱스라면
-		if(idx == m - 1) {
-			for(int i = 0; i < m; i++)
-				sb.append(arr[i] + " ");
-			sb.append("\n");
-			// 다시 방문 체크 풀어줌
-			visited[num] = false;
-			return;
-		}
-		
-		// 방문한 적 없는 수면 재귀
-		for(int i = 1; i <= n; i++) {
-			if(!visited[i])
-				progression(idx + 1, i);
-		}
-		
-		// 다시 방문 체크 풀어줌
-		visited[num] = false;
-	}
 
-	public static void main(String[] args) throws Exception{
+    static int N, M;
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder result;
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		// n, m 입력
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		
-		// 수열을 담을 배열 선언
-		arr = new int[m];
-		// 방문체크 하기 위한 배열 선언
-		visited = new boolean[n + 1];
-		
-		// 수열 구하기
-		sb = new StringBuilder();
-		for (int i = 1; i <= n; i++) {
-			progression(0, i);
-		}
-		
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws Exception{
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M + 1];
+        visited = new boolean[N + 1];
+        result = new StringBuilder();
+
+        combi(1);
+
+        System.out.print(result);
+    }
+
+    private static void combi(int len) {
+        if(len == M + 1) {
+            for (int i = 1; i <= M; i++) {
+                result.append(arr[i]).append(" ");
+            }
+            result.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                arr[len] = i;
+                combi(len + 1);
+                visited[i] = false;
+            }
+        }
+    }
 }
