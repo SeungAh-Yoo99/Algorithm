11d6a219af2ad6c4b184004b7f1cd8deb31bee82
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2025-12-15 23:12:16 +0900

    Update 튜플.java

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -12,9 +12,12 @@ class Solution {
                 start = i;
                 while(s.charAt(++i) != '}');
                 end = i;
+                // {} 안의 수들을 ","를 기준으로 분할
                 st = new StringTokenizer(s.substring(start + 1, end), ",");
+                // 분할한 수들을 리스트에 담기
                 list = new ArrayList<>();
                 while(st.hasMoreTokens()) list.add(Integer.parseInt(st.nextToken()));
+                // 리스트 길이를 key값으로 Map에 담기
                 map.put(list.size(), list);
             }
         }
@@ -22,8 +25,10 @@ class Solution {
         int[] answer = new int[map.size()];
         Set<Integer> set = new HashSet<>();
         for(int i = 1; i <= map.size(); i++) {
+            // 리스트 길이가 짧은 순으로 가져오기
             list = map.get(i);
             for(int j = 0; j < list.size(); j++) {
+                // 이미 확인한 수는 넘어가기
                 if(!set.contains(list.get(j))) {
                     set.add(list.get(j));
                     answer[i - 1] = list.get(j);
