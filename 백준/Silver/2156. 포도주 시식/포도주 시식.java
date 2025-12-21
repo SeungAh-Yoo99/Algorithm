93be22917962667aa22adf869c9fd7c0109f8f63
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2025-12-21 20:30:39 +0900

    [Silver I] Title: 포도주 시식, Time: 100 ms, Memory: 13736 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,28 +1,40 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) throws Exception{
 
-		Scanner scan=new Scanner(System.in);
-		int n=scan.nextInt();
-		int arr[]=new int[10001];
-		int dp[]=new int[10001];
-		
-		for (int i=1;i<=n;i++)
-			arr[i]=scan.nextInt();
-		
-		dp[1]=arr[1];
-		dp[2]=dp[1]+arr[2];
-		for (int i=3;i<=n;i++) {
-			dp[i]=Math.max(dp[i-3]+arr[i-1]+arr[i],dp[i-2]+arr[i]);
-			dp[i]=Math.max(dp[i-1], dp[i]);
-		}
-		
-		int max=0;
-		for (int i=n;i>0;i--)
-			max=Math.max(max, dp[i]);
-		System.out.println(max);
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int n = Integer.parseInt(br.readLine());
+
+        int[] wines = new int[n];
+        for (int i = 0; i < n; i++) {
+            wines[i] = Integer.parseInt(br.readLine());
+        }
+
+        // dp[i][0] := 현재 와인 마시지 않음
+        // dp[i][1] := 현재 와인 마심 - 앞 단계 와인 마시지 않음
+        // dp[i][2] := 현재 와인 마심 - 앞 단계 와인 마심
+        int[][] dp = new int[n][3];
+        dp[0][0] = 0;
+        dp[0][1] = wines[0];
+        dp[0][2] = wines[0];
+
+        for (int i = 1; i < n; i++) {
+
+            // 현재 와인 마시지 않는 경우 - 이전 단계애서 가장 큰 수 저장
+            dp[i][0] = Math.max(Math.max(dp[i - 1][0], dp[i - 1][1]), dp[i - 1][2]);
+
+            // 현재 와인 마시는 경우 - 앞 와인 안마심
+            dp[i][1] = dp[i - 1][0] + wines[i];
+
+            // 현재 와인 마시는 경우 - 앞 와인 마심
+            dp[i][2] = dp[i - 1][1] + wines[i];
+        }
+
+        // 마지막 단계 중 가장 큰 수 출력
+        System.out.println(Math.max(Math.max(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]));
+    }
 }
